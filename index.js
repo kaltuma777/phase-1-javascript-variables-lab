c50f9f14cdf1c16f52d1fa34ba2408b3cb2bde3c
Kaltuma777 <Kaltuma.abdi@student.moringaschool.com>
2023-10-02 16:24:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const companyName = 'Scuber'
+mostProfitableNeighborhood = 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea'
+companyCeo = 'Susan Smith'
+let companyCeo = 'Susan Smith'
+
+
+
+
+
+
+
